21caff41ae99afaec088b0f9872a9034576666fd
Amit Zahy <amitzahy@TM-Amit-Zahy-MacBook.local>
2026-01-27 15:53:18 +0200

    Updates

---
+++ assets/index-vGFORQlH.js
@@ -0,0 +1,1 @@
+import{z as r}from"./index-B9akm6uP.js";var a=r();export{a as r};
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/TravelPlannerAI/sw.js', { scope: '/TravelPlannerAI/' })})}
