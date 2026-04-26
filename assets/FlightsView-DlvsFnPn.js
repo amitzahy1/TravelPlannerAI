103fd64eadcde41d8c1aa82bc46c9a09a9d0dd28
Amit Zahy <amitzahy@TM-Amit-Zahy-MacBook.local>
2026-04-26 14:38:18 +0300

    Updates

--- assets/FlightsView-DlvsFnPn.js
+++ assets/FlightsView-DlvsFnPn.js
@@ -1,4 +1,4 @@
-import{c as U,r as N,s as be,j as e,T as ie,X as de,M as fe,C as ce,a as ee,P as xe,b as q,S as J,d as se,F as pe,L as me,e as ge,f as je,g as ve,t as W,A as Ne,m as ye,l as re}from"./index-DFKFeQ_d.js";import{P as ue}from"./pen-C710E5kT.js";import{C as we}from"./ConfirmModal-BJleNi1K.js";/**
+import{c as U,r as N,s as be,j as e,T as ie,X as de,M as fe,C as ce,a as ee,P as xe,b as q,S as J,d as se,F as pe,L as me,e as ge,f as je,g as ve,t as W,A as Ne,m as ye,l as re}from"./index-BJviuTyn.js";import{P as ue}from"./pen-DE_Lrvlh.js";import{C as we}from"./ConfirmModal-BqzNAJkh.js";/**
  * @license lucide-react v0.562.0 - ISC
  *
  * This source code is licensed under the ISC license.
